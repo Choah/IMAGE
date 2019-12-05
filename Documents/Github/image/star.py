70fac29affef5b3f0b0fd7dddc770f116ede95be
Gyungah Cho <hellen110@naver.com>
2019-12-05 20:41:53 +0900

    image

---
+++ Documents/Github/image/star.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# * _언더바 9가지 
+# 
+# 1. 
+
+# In[1]:
+
+
+a = 1
+_a =2 
+
+
+# In[2]:
+
+
+def b():
+    return 1 
+
+
+# In[3]:
+
+
+def _b():
+    return 2 
+
+
+# In[ ]:
+
+
+__all__ = ['a']
+
+
